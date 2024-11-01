6772b12c841def1296c44c5099e9a5dfcb61dc39
kenro-t <ngkn1425@gmail.com>
2024-11-01 14:14:30 +0000

    hot reload再設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // hot reload
+  webpack: (config, context) => {
+    config.watchOptions = {
+      ignored: /node_modules/,
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
