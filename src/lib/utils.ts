05a152b14bf0b319fc05489286a6334f732c8f1e
kenro-t <ngkn1425@gmail.com>
2024-11-15 14:51:53 +0000

    fetchData

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,29 +1,67 @@
-
 // TODO: DOMParserで利用できるよう修正したい
 export const extractTags = (htmlString) => {
-    return htmlString.replace(/<[^>]*>/g, '');
-}
+  return htmlString.replace(/<[^>]*>/g, "");
+};
 
 export const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const months = [
-        "January", "February", "March", "April", "May", "June", 
-        "July", "August", "September", "October", "November", "December"
-    ];
-    
-    const month = months[date.getMonth()];
-    const day = date.getDate();
-    const year = date.getFullYear();
-    
-    return `${month} ${day}, ${year}`;
-}
+  const date = new Date(dateString);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+};
 
 // 文字列を丸めて…を付けて表示する
 export const truncateContent = (contentText) => {
-    const maxLength = 100;
-    const suffix = '...';
-    if (contentText.length > maxLength) {
-        return contentText.slice(0, maxLength) + suffix;
+  const maxLength = 100;
+  const suffix = "...";
+  if (contentText.length > maxLength) {
+    return contentText.slice(0, maxLength) + suffix;
+  }
+  return contentText;
+};
+
+export const fetchData = async (
+  url: string,
+  method: string = "GET",
+  body?: any,
+  token?: string
+) => {
+  const options: RequestInit = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      // ...body(token &&)
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+
+    if (!res.ok) {
+      throw new Error(`error :${res.status}`);
     }
-    return contentText
-}
+    const data = await res.json();
+    return data;
+
+  } catch (error) {
+    console.error("Fetch error " + error);
+    throw error;
+  }
+};
